ac5d873343abeeb272ec3a1008ff178d434ea971
yogapavithra <pavithra210605@gmail.com>
2025-03-12 07:05:38 -0700

    script.js file added

---
+++ script.js
@@ -0,0 +1,48 @@
+// Toggle icon and navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+// Scroll sections
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        }
+    });
+
+    // Sticky header
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    // Remove toggle icon and hide navbar when a navbar link is clicked
+    navLinks.forEach(link => {
+        link.onclick = () => {
+            menuIcon.classList.remove('bx-x');
+            navbar.classList.remove('active');
+        };
+    });
+
+     // You can add logic here for animating the footer based on scroll
+};
+
+let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
